05ea155fd494494fe7b9cebe60729e8e236608e6
silence1984 <zain.svip@gmail.com>
2019-11-01 17:59:09 +0800

    fish

---
+++ fish/config.fish
@@ -0,0 +1,11 @@
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /home/lzy/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
+thefuck --alias | source
+
+#alias ls='ls --color=auto'
+alias mtwin='sudo mount /dev/nvme0n1p2 /mnt/winc/'
+alias fuxk='fuck'
