15f25084067b8c159be178553bbb550b6a09a174
silence1984 <zain.svip@gmail.com>
2019-11-07 21:02:23 +0800

    emmmm

--- fish/config.fish
+++ fish/config.fish
@@ -7,5 +7,12 @@ eval /home/lzy/anaconda3/bin/conda "shell.fish" "hook" $argv | source
 thefuck --alias | source
 
 #alias ls='ls --color=auto'
+
 alias mtwin='sudo mount /dev/nvme0n1p2 /mnt/winc/'
 alias fuxk='fuck'
+
+export WIN_PATH=/mnt/winc/Users/LZY
+
+export LC_ALL=en_US.UTF-8
+
+export LANG=en_US.UTF-8
